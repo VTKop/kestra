ffe29a37b36f2b255fc55041a40013c0d847c127
Tijn Koppert <tijnkoppert@gmail.com>
2025-04-02 13:05:44 +0200

    Building fix

--- ui/plugins/filename.ts
+++ ui/plugins/filename.ts
@@ -1,8 +1,10 @@
 import type {Plugin} from "vite";
 
 const process = (path: string): string => {
-    const match = path.match(/.*\/src\/(.*)\.vue$/);
-    return match ? match[1].toLowerCase() : path;
+    // Replace backslashes with forward slashes
+    const normalizedPath = path.replace(/\\/g, "/");
+    const match = normalizedPath.match(/.*\/src\/(.*)\.vue$/);
+    return match ? match[1].toLowerCase() : normalizedPath;
 };
 
 export const filename = (): Plugin => {
